e2d7f41c22493fc78c8435713ff93fb0f3497e72
John Reeves <j.reeves@garvan.org.au>
2024-11-18 15:25:30 +1100

    Add run mod script

---
+++ run_mod.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -e
+eval  "$(conda shell.bash hook)"
+conda activate snakemake_7.32.4
+
+module=genotyping
+
+if [[ "$(hostname)" == *"nci"* ]]; then
+    echo "Running on NCI"
+    global_profile="--profile modules/$module/profiles/global/nci_a56"
+    workflow_profile="--workflow-profile modules/$module/profiles/workflow "
+    module load singularity
+    mkdir -p logs/joblogs
+else
+    echo "WARNING: Unknown host: $(hostname)"
+    echo "WARNING: No known global profile for this host"
+    echo "WARNING: Running without global profile"
+    echo "See https://github.com/swarbricklab/snakemake_config/blob/main/README.md"
+    global_profile=""
+fi
+
+snakemake $global_profile $workflow_profile \
+    --snakefile modules/$module/workflow/Snakefile \
+    --configfile config/$module/config.yaml \
+    $@
